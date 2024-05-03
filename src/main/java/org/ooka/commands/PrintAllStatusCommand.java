ab3c62a92c8b8f6aa00cbed5c8f4e9f562db1bf9
LeonHeimermann <leonheimermann@gmail.com>
2024-05-03 11:09:23 +0200

    Added feature to get status of runtime/component

---
+++ src/main/java/org/ooka/commands/PrintAllStatusCommand.java
@@ -0,0 +1,27 @@
+package org.ooka.commands;
+
+import org.ooka.runtime.Runtime;
+
+import java.util.List;
+
+public class PrintAllStatusCommand implements Command {
+
+    Runtime runtime;
+    int componentId;
+
+    public PrintAllStatusCommand(Runtime runtime, int componentId) {
+        this.runtime = runtime;
+        this.componentId = componentId;
+    }
+
+    @Override
+    public boolean execute() {
+        List<String> statusList = runtime.getAllStatus();
+        if (statusList .isEmpty()) {
+            return false;
+        }
+        statusList.forEach(System.out::println);
+        return true;
+    }
+
+}
--- src/main/java/org/ooka/types/State.java
+++ src/main/java/org/ooka/types/State.java
@@ -1,9 +1,19 @@
 package org.ooka.types;
 
 public enum State {
-    INITIAL,
-    LOADED,
-    RUNNING,
-    PAUSED,
-    STOPPED
+    INITIAL("initial"),
+    LOADED("loaded"),
+    RUNNING("running"),
+    PAUSED("paused"),
+    STOPPED("stopped");
+
+    private final String label;
+
+    State(String label) {
+        this.label = label;
+    }
+
+    public String toString() {
+        return label;
+    }
 }
