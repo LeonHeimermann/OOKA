5ad5a65df3161629c4258f5d942ca63fa200a2ad
LeonHeimermann <leonheimermann@gmail.com>
2024-05-09 14:35:49 +0200

    Change handling of input commands

--- lzu/src/main/java/org/ooka/cli/CliImpl.java
+++ lzu/src/main/java/org/ooka/cli/CliImpl.java
@@ -1,6 +1,7 @@
 package org.ooka.cli;
 
 import org.ooka.commands.*;
+import org.ooka.runtime.Runtime;
 import org.ooka.runtime.RuntimeImpl;
 
 import java.util.Scanner;
@@ -8,70 +9,43 @@ import java.util.Scanner;
 public class CliImpl implements Cli {
     @Override
     public void start() {
-        var runtime = RuntimeImpl.getInstance();
         System.out.println("--- Beste LZU der Welt ---");
         printHelp();
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
+            String command = scanner.nextLine();
+            handleCommand(command);
+        }
+    }
 
-            if (input.equals("help")) {
+    private void handleCommand(String command) {
+        Runtime runtime = RuntimeImpl.getInstance();
+        String[] inputTokens = command.split(" ", 2);
+        switch (inputTokens[0]) {
+            case "help":
                 printHelp();
-            } else if (input.equals("exit")) {
-                System.out.println("LZU wird heruntergefahren...");
-                System.exit(0);
-            } else if (input.startsWith("deploy")) {
-                var parameter = input.split("\\s+");
-                if (parameter.length != 2) {
-                    System.out.println("deploy erwartet genau eine pfadangabe");
-                } else {
-                    if (executeCommand(new LoadJarCommand(runtime, parameter[1]))) {
-                        System.out.println("successfully deployed component");
-                    }
-                }
-            } else if (input.startsWith("start")) {
-                var parameter = input.split("\\s+");
-                if (parameter.length != 2) {
-                    System.out.println("start erwartet genau eine komponenten-id");
-                } else {
-                    try {
-                        int componentId = Integer.parseInt(parameter[1]);
-                        Command startInstanceCommand = new StartInstanceCommand(runtime, componentId);
-                        startInstanceCommand.execute();
-                        System.out.println("Successfully started component");
-                    } catch (NumberFormatException e) {
-                        System.out.println("Id could not be parsed");
-                    }
-                }
-            } else if (input.equals("list")) {
+                break;
+            case "exit":
+                handleExit(runtime);
+                break;
+            case "deploy":
+                handleDeploy(inputTokens, runtime);
+                break;
+            case "start":
+                handleStart(inputTokens, runtime);
+                break;
+            case "list":
                 executeCommand(new ListComponentsCommand());
-            } else if (input.startsWith("status")) {
-                var parameter = input.split("\\s+");
-                if (parameter.length == 1) {
-                    Command printAllStatusCommand = new PrintAllStatusCommand(runtime);
-                    printAllStatusCommand.execute();
-                } else {
-                    int componentId = Integer.parseInt(parameter[1]);
-                    Command printStatusCommand = new PrintStatusCommand(runtime, componentId);
-                    printStatusCommand.execute();
-                }
-            } else if (input.startsWith("remove")) {
-                var parameter = input.split("\\s+");
-                if (parameter.length != 2) {
-                    System.out.println("remove erwartet genau eine komponenten-id");
-                } else {
-                    try {
-                        int componentId = Integer.parseInt(parameter[1]);
-                        Command removeComponentCommand = new RemoveComponentCommand(runtime, componentId);
-                        removeComponentCommand.execute();
-                        System.out.println("Successfully removed component");
-                    } catch (NumberFormatException e) {
-                        System.out.println("Id could not be parsed");
-                    }
-                }
-            } else {
-                System.out.println("'" + input + "' ist kein gültiges kommando. für eine Kommandoübersicht 'help' eingeben.");
-            }
+                break;
+            case "status":
+                handleStatus(inputTokens, runtime);
+                break;
+            case "remove":
+                handleRemove(inputTokens, runtime);
+                break;
+            default:
+                System.out.println("'" + command + "' ist kein gültiges kommando. für eine Kommandoübersicht 'help' eingeben.");
+                break;
         }
     }
 
@@ -89,5 +63,62 @@ public class CliImpl implements Cli {
         System.out.println("remove [id]: \t\t\tKomponente entfernen");
         System.out.println("help:\t\t\tHilfe anzeigen");
         System.out.println("exit:\t\t\tLZU beenden");
+    }
+
+    private void handleExit(Runtime runtime) {
+        System.out.println("LZU wird heruntergefahren...");
+        executeCommand(new ShutdownCommand(runtime));
+        System.exit(0);
+    }
+
+    private void handleDeploy(String[] parameter, Runtime runtime) {
+        if (parameter.length != 2) {
+            System.out.println("deploy erwartet genau eine pfadangabe");
+        } else {
+            if (executeCommand(new LoadJarCommand(runtime, parameter[1]))) {
+                System.out.println("successfully deployed component");
+            }
+        }
+    }
+
+    private void handleStart(String[] parameter, Runtime runtime) {
+        if (parameter.length != 2) {
+            System.out.println("start erwartet genau eine komponenten-id");
+        } else {
+            try {
+                int componentId = Integer.parseInt(parameter[1]);
+                Command startInstanceCommand = new StartInstanceCommand(runtime, componentId);
+                startInstanceCommand.execute();
+                System.out.println("Successfully started component");
+            } catch (NumberFormatException e) {
+                System.out.println("Id could not be parsed");
+            }
+        }
+    }
+
+    private void handleStatus(String[] parameter, Runtime runtime) {
+        if (parameter.length == 1) {
+            Command printAllStatusCommand = new PrintAllStatusCommand(runtime);
+            printAllStatusCommand.execute();
+        } else {
+            int componentId = Integer.parseInt(parameter[1]);
+            Command printStatusCommand = new PrintStatusCommand(runtime, componentId);
+            printStatusCommand.execute();
+        }
+    }
+
+    private void handleRemove(String[] parameter, Runtime runtime) {
+        if (parameter.length != 2) {
+            System.out.println("remove erwartet genau eine komponenten-id");
+        } else {
+            try {
+                int componentId = Integer.parseInt(parameter[1]);
+                Command removeComponentCommand = new RemoveComponentCommand(runtime, componentId);
+                removeComponentCommand.execute();
+                System.out.println("Successfully removed component");
+            } catch (NumberFormatException e) {
+                System.out.println("Id could not be parsed");
+            }
+        }
     }
 }
